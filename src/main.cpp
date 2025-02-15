c8b814393676ca33d54bd7aa8c69ec2b8a3f98b4
Femi Sharomi <Mrfemi1998@gmail.com>
2025-02-15 16:30:57 +0000

    Project structure setup

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13 Feb 2025
+ *      Author: femi
+ */
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
+
